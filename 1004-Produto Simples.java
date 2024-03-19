013d5d5a8233e69e011c5c15b02d672586433b5f
Marcos Vinicius <131707709+MarvVG@users.noreply.github.com>
2024-03-19 08:53:57 -0300

    Create 1004-Produto Simples.java

---
+++ 1004-Produto Simples.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){     
+        int a,b,prod;
+        Scanner scan = new Scanner(System.in);
+        a= scan.nextInt();
+        b= scan.nextInt();
+        prod = a*b;
+        System.out.println("PROD = "+prod);
+    }
+}
