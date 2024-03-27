b429749248e07ac5838c4b1742aa28a7e8974e5e
Marcos Vinicius <131707709+MarvVG@users.noreply.github.com>
2024-03-27 18:04:16 -0300

    Create  1008- Salário.java

---
+++ 1008- Salário.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){     
+        int A,B;
+        float C,salario;
+        Scanner scan = new Scanner(System.in);
+        A= scan.nextInt();
+        B= scan.nextInt();
+        C= scan.nextFloat();
+        salario = C * B;
+        System.out.println("NUMBER = "+A);
+        System.out.printf("SALARY = U$ %.2f",salario);
+        System.out.println();
+    }
+}
